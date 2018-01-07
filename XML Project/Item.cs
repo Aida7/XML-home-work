7b8953cf4f1a5eb31f663ad0de8c1f30fd8bcbb5
Aida7 <33822250+Aida7@users.noreply.github.com>
2018-01-07 22:21:53 +0600

    Добавьте файлы проекта.

---
+++ XML Project/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Project
+{
+  public class Item
+    {
+        //: Title, Link, Description, PubDate.
+        public string Title { get; set; } //заглавие 
+        public string Link { get; set; } // сылка
+        public string Description { get; set; }  //описание 
+        public string PubDate { get; set; } //дата публикций 
+
+    }
+}
---
+++ XML Project/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace XML_Project
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> list = new List<Item>();
+            Item item = new Item();
+            String xmlString = File.ReadAllText("XML.xml");
+            XmlDocument document = new XmlDocument();
+            document.Load("XML.xml");
+            var root = document.DocumentElement;
+            foreach (XmlNode child in root.ChildNodes)
+            {
+                item.Title = root.Attributes["news"].InnerXml;
+                item.Link = root.Attributes["link"].InnerText;
+                item.Description = root.Attributes["description"].InnerText;
+                item.PubDate = root.Attributes["pubDate"].InnerText;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
